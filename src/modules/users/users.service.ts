8161c85b80744ae43dbd47806fd550a8a3b013fc
Aruberth <alberthqwertyk@gmail.com>
2026-04-03 20:28:48 -0300

    feat: enhance user module with validation and error handling

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,29 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Prisma } from 'generated/prisma/client';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(createUserDto: CreateUserDto) {
+    try {
+      return await this.prisma.user.create({
+        data: createUserDto,
+        select: {
+          id: true,
+          username: true,
+          name: true,
+          email: true,
+          biography: true,
+          createdAt: true,
+        },
+      });
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          const target = error.meta?.target as string[] | undefined;
+
+          const field =
+            target?.[0] ??
+            (error.message.includes('username') ? 'username' : null) ??
+            (error.message.includes('email') ? 'email' : null);
+
+          const messages: Record<string, string> = {
+            email: 'Email already registered',
+            username: 'Username already registered',
+          };
+
+          throw new ConflictException(
+            (field && messages[field]) ?? 'Duplicate field',
+          );
+        }
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return this.prisma.user.findMany();
-  }
+  async findById(id: number) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+        biography: true,
+        createdAt: true,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+    if (!user) throw new NotFoundException('User not Found');
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
+    //validate
+
+    //verify if have
+
+    //changue and/or return message
     return `This action updates a #${id} user`;
   }
 
   remove(id: number) {
+    //validate
+
+    //verify if have
+
+    //delete and/or return message
+
     return `This action removes a #${id} user`;
   }
+
+  getAllUserPosts(id: number) {
+    // validate
+    //return response
+  }
+
+  getAllUserComments(id: number) {}
 }
